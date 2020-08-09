daf695dd32f3183f5d2bedbd17a9e4db5fb341ab
mahdieh.parhizkari <Mahdieh.Parhizkari@hotmail.com>
2020-08-09 22:30:14 +0430

    pure version

---
+++ src/main/java/com/ourbizfmly/billreg/billregistration/BillregistrationApplication.java
@@ -0,0 +1,13 @@
+package com.ourbizfmly.billreg.billregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillregistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ourbizfmly/billreg/billregistration/BillregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourbizfmly.billreg.billregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
