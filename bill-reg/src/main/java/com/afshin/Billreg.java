8876d050697e4a2dd81e690127cda86d01350333
afshin.parhizkari@gmail.com <afshin.parhizkari@gmail.com>
2023-08-03 18:01:13 +0330

    add structure

---
+++ bill-reg/src/main/java/com/afshin/Billreg.java
@@ -0,0 +1,11 @@
+package com.afshin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Billreg {
+    public static void main(String[] args) {
+        SpringApplication.run(Billreg.class, args);
+    }
+}
